f2cde453676762bf93f3cb0c9388253692957c95
lingyuzeng <pylyzeng@gmail.com>
2023-08-01 17:00:09 +0800

    update4

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,10 @@ if [ -n "$APPTAINER_KEY" ]; then
     mkdir -p ~/.apptainer/keys && echo "${APPTAINER_KEY}" | base64 -d > ~/.apptainer/keys/pgp-secret
 fi
 
+# Build apptainer image from kaczmarj/apptainer-in-docker Dockerfile
+git clone https://github.com/kaczmarj/apptainer-in-docker
+docker build -t hotwa/input:apptainer -f Dockerfile apptainer-in-docker/
+
 # Convert Docker image
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/work hotwa/input:apptainer build ${INPUT_DOCKERFILE_PATH}-${TIMESTAMP}.sif docker-daemon://hotwa/input:latest
 

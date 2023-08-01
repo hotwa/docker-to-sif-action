418de899e467311f4a46b721f8e1d883d3e427da
lingyuzeng <pylyzeng@gmail.com>
2023-08-01 17:28:49 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,3 +28,6 @@ fi
 
 # Package SIF file
 tar czvf ${INPUT_DOCKERFILE_PATH}-${TIMESTAMP}.sif.tar.gz ${INPUT_DOCKERFILE_PATH}-${TIMESTAMP}.sif
+
+# Move the tar.gz file to the workspace directory
+mv ${INPUT_DOCKERFILE_PATH}-${TIMESTAMP}.sif.tar.gz /github/workspace/
